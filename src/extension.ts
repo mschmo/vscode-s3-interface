5953703a4f23f28c9ff843addb263d0ed815a44b
Matthew Schmoyer <mattschmo@gmail.com>
2017-04-09 22:02:24 -0600

    WIP Only activate when triggered from command

--- src/extension.ts
+++ src/extension.ts
@@ -7,42 +7,46 @@ import { S3API } from './s3-api';
 
 
 export function activate(context: ExtensionContext) {
-    var config = workspace.getConfiguration('s3Interface');
-    // any(config[k] is None for k in ('accessKeyId', 'secretAcessKey', 'bucketName'))
-    if (config.accessKeyId === null || config.secretAccessKey === null || config.bucketName === null) {
-        // TODO: Specify which value(s) is missing
-        window.showErrorMessage('Must provide access key, secret, and bucket name in settings.');
-        return;
-    }
-
-    // Get all the objects and register provider with BucketContents structure.
-    // I think the data structure of BucketContents shouls be something like a B-Tree.
-    // But I don't know what a B-Tree is.
-    // I'll Google it later.
-    var s3 = new S3API(config.accessKeyId, config.secretAccessKey, config.bucketName, config.region);
-    s3.setContents().then(contents => {
-        window.registerTreeExplorerNodeProvider('s3NodeProvider', new S3NodeProvider(contents));
-    });
-
-    const commandRegistration = commands.registerCommand('s3Interface.clickedFile', node => {
-        if (node.is_dir) {
+    var disposable = commands.registerCommand('s3Interface.loadS3', () => {
+        var config = workspace.getConfiguration('s3Interface');
+        // any(config[k] is None for k in ('accessKeyId', 'secretAcessKey', 'bucketName'))
+        if (config.accessKeyId === null || config.secretAccessKey === null || config.bucketName === null) {
+            // TODO: Specify which value(s) is missing
+            window.showErrorMessage('Must provide access key, secret, and bucket name in settings.');
             return;
         }
 
-        // Should check first if file exists, to not repeat the request
-        s3.getFileContents(node.key).then(body => { 
-            var provider = new S3ContentProvider(body);
-            workspace.registerTextDocumentContentProvider(S3ContentProvider.scheme, provider);
-            var uri = Uri.parse(`${ S3ContentProvider.scheme }:${ node.name }?key=${ node.key }`);
-            var document = workspace.openTextDocument(uri);
-            return document.then(doc => {
-                window.showTextDocument(doc)
+        // Get all the objects and register provider with BucketContents structure.
+        // I think the data structure of BucketContents shouls be something like a B-Tree.
+        // But I don't know what a B-Tree is.
+        // I'll Google it later.
+        var s3 = new S3API(config.accessKeyId, config.secretAccessKey, config.bucketName, config.region);
+        s3.setContents().then(contents => {
+            window.registerTreeExplorerNodeProvider('s3NodeProvider', new S3NodeProvider(contents));
+        });
+
+        const commandRegistration = commands.registerCommand('s3Interface.clickedFile', node => {
+            if (node.is_dir) {
+                return;
+            }
+
+            // Should check first if file exists, to not repeat the request
+            s3.getFileContents(node.key).then(body => { 
+                var provider = new S3ContentProvider(body);
+                workspace.registerTextDocumentContentProvider(S3ContentProvider.scheme, provider);
+                var uri = Uri.parse(`${ S3ContentProvider.scheme }:${ node.name }?key=${ node.key }`);
+                var document = workspace.openTextDocument(uri);
+                return document.then(doc => {
+                    window.showTextDocument(doc)
+                });
             });
         });
+
+        context.subscriptions.push(
+		    commandRegistration
+	    );
     });
-    context.subscriptions.push(
-		commandRegistration
-	);
+    context.subscriptions.push(disposable);
 }
 
 class S3NodeProvider implements TreeExplorerNodeProvider<BucketNode> {
